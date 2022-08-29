bec73d93e8f5cbd13d40f2aeef5349ecd43b6a1b
Arzuman-hue <arz.arzum@mail.ru>
2022-08-29 21:34:45 +0400

    add new files

--- src/components/childContext/childContext.js
+++ src/components/childContext/childContext.js
@@ -1,15 +1,2 @@
-import React from 'react';
-import {useContext} from "react";
-import {UserDataContext} from "../sign-up/sign-up";
 
- export function ChildContext() {
 
-     const userData = useContext(UserDataContext)
-
-    return (
-        <div>
-            <p>This is user Name</p>
-            {userData.name}
-        </div>
-    )
-}
